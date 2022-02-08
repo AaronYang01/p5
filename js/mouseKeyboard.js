59074f6fb5ad466ace9ddcbc8c876001493b2339
AaronYang01 <98726394+AaronYang01@users.noreply.github.com>
2022-02-08 12:27:09 -0600

    Add files via upload

---
+++ js/mouseKeyboard.js
@@ -0,0 +1,21 @@
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+
+}
+function draw(){
+  background(0,0,0);
+  fill ('gray');
+  ellipse(150,150,150,150);
+
+  fill ('green');
+  rect(400,250,150,150);
+  rect(800,250,150,150);
+
+  fill(162,82,45);
+  rect(450,400,50,1000);
+  rect(850,400,50,1000);
+
+
+
+
+}
